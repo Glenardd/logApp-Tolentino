57fb71b225b71bbecd6e6522612232918766ad5d
unknown <tolentinojanglenard5@gmail.com>
2022-10-12 00:04:30 +0800

    added some lines

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
 	// Create Connection
-	$DB_HOST = "localhost";
-	$DB_USER = "root";
-	$DB_PASS = "";
-	$DB_NAME = "personal_informations";
+	$DB_HOST = "sql6.freesqldatabase.com";
+	$DB_USER = "sql6525697";
+	$DB_PASS = "7bCzTmbZmD";
+	$DB_NAME = "sql6525697";
 	$conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
 	// Check Connection
