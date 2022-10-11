bd27c7dff66692bdda01e88384e0d8debb7a235f
unknown <tolentinojanglenard5@gmail.com>
2022-10-11 23:48:02 +0800

    added files

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', '');
+	define('DB_HOST', ' sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6525697');
+	define('DB_PASS', '7bCzTmbZmD');
+	define('DB_NAME', 'sql6525697');
